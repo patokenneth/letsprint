33dbc2ca124c92e39af4131b554878e2b713b595
patokenneth <chiemezieoloto4@gmail.com>
2021-12-12 17:20:01 +0100

    Add Context

---
+++ src/letsprint/Model/Order.cs
@@ -0,0 +1,19 @@
+﻿using letsprint.Utility;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace letsprint.Model
+{
+    public class Order
+    {
+        public Guid OrderID => Guid.NewGuid();
+
+        public ProductType ProductType { get; set; }
+
+        public int Quantity { get; set; }
+
+        public float RequiredBinWidth { get; set; }
+    }
+}
